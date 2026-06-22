5f23809f833a2473b11567208c932442a6338d7b
rahat-4 <mainul.rahat4@gmail.com>
2026-06-22 12:32:06 +0600

    Remove unused code

--- core/api/urls/__init__.py
+++ core/api/urls/__init__.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("auth", include("api.urls.auth")),
     path("organisation", include("api.urls.organisation")),
-    path("users", include("api.urls.users")),
     path("property", include("api.urls.property")),
 ]
